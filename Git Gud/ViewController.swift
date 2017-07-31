ad2b0bee2f9e77fa79ecc9c3668f580e8d009309
iD Student <idstudent@stratton-two-twenty-three.mit.edu>
2017-07-31 16:36:40 -0400

    Initial Commit

---
+++ Git Gud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Gud
+//
+//  Created by iD Student on 7/31/17.
+//  Copyright © 2017 iD Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
